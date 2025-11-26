77eb4829196ec4e729206993ae4fa6f0a24b87f3
Paolo Josue Coaquira Anccori <paolojosuecoaquiraanccori@gmail.com>
2025-11-26 18:51:15 -0500

    1

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,9 +1,21 @@
+
+import java.util.List;
+
 public class Hospital {
     private String nombreHospital;
     private String direccion;
     private String telefono;
     private String email;
     private int capacidadCamas;
+    private List<Doctor> doctores;
+    private List<Paciente> pacientes;
+    private List<Enfermera> enfermeras;
+    private List<Administrador> administradores;
+    private List<Farmacia> farmacias;
+    private List<Departamento> departamentos;
+
+
+    //Contructor
     public Hospital(String nombreHospital, String direccion, String telefono, String email, int capacidadCamas) {
         this.nombreHospital = nombreHospital;
         this.direccion = direccion;
@@ -11,6 +23,8 @@ public class Hospital {
         this.email = email;
         this.capacidadCamas = capacidadCamas;
     }
+
+    //Get y set
     public String getNombreHospital() {
         return nombreHospital;
     }
