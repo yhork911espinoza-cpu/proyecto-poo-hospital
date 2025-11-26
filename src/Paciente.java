50d29f365ab5446edd5bacaa4f610864e764f402
Paolo Josue Coaquira Anccori <paolojosuecoaquiraanccori@gmail.com>
2025-11-25 19:09:33 -0500

    3

--- src/Paciente.java
+++ src/Paciente.java
@@ -10,16 +10,67 @@ public class Paciente extends Usuario{
     private Doctor doctorAsignado;
 
     //Constructor/s de paciente
-
-    public Paciente(List<String> alergias, String grupoSanguineo, String numeroHistoriaClinica, 
-                    String seguroMedico, String direccion, String email, LocalDate fechaNacimiento, 
-                    String genero, String nombre, String primerApellido, String segundoApellido,
-                    String telefono) {//----------------------------------------------------------  
-        super(direccion, email, fechaNacimiento, genero, nombre, primerApellido, segundoApellido, telefono);
-        this.alergias = alergias;
-        this.grupoSanguineo = grupoSanguineo;
+    
+    public Paciente(String nombre, String primerApellido, String segundoApellido, String direccion, String telefono, String email, LocalDate fechaNacimiento, String genero, //de la clase heredada usuario
+                    String numeroHistoriaClinica, String grupoSanguineo, List<String> alergias, String seguroMedico, EstadoPaciente estadoPaciente, Doctor doctorAsignado) {
+        super(nombre, primerApellido, segundoApellido, direccion, telefono, email, fechaNacimiento, genero);
         this.numeroHistoriaClinica = numeroHistoriaClinica;
+        this.grupoSanguineo = grupoSanguineo;
+        this.alergias = alergias;
+        this.seguroMedico = seguroMedico;
+        this.estadoPaciente = estadoPaciente;
+        this.doctorAsignado = doctorAsignado;
+    }
+
+    //get y set de paciente
+
+    public String getNumeroHistoriaClinica() {
+        return numeroHistoriaClinica;
+    }
+
+    public void setNumeroHistoriaClinica(String numeroHistoriaClinica) {
+        this.numeroHistoriaClinica = numeroHistoriaClinica;
+    }
+
+    public String getGrupoSanguineo() {
+        return grupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String grupoSanguineo) {
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    public List<String> getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(List<String> alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getSeguroMedico() {
+        return seguroMedico;
+    }
+
+    public void setSeguroMedico(String seguroMedico) {
         this.seguroMedico = seguroMedico;
     }
+
+    public EstadoPaciente getEstadoPaciente() {
+        return estadoPaciente;
+    }
+
+    public void setEstadoPaciente(EstadoPaciente estadoPaciente) {
+        this.estadoPaciente = estadoPaciente;
+    }
+
+    public Doctor getDoctorAsignado() {
+        return doctorAsignado;
+    }
+
+    public void setDoctorAsignado(Doctor doctorAsignado) {
+        this.doctorAsignado = doctorAsignado;
+    }
+
     
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,8 +1,7 @@
 import java.time.LocalDate;
-import java.util.UUID;
 
 public abstract class Usuario {
-    private String idUsuario;
+    private String idUsuario; /// pensando en eso
     private String nombre;
     private String primerApellido;
     private String segundoApellido;
@@ -13,18 +12,92 @@ public abstract class Usuario {
     private String genero;
 
     //Contructor de usuarios
-    public Usuario(String direccion, String email, LocalDate fechaNacimiento, String genero, String nombre, String primerApellido, String segundoApellido, String telefono) {
-        this.direccion = direccion;
-        this.email = email;
-        this.fechaNacimiento = fechaNacimiento;
-        this.genero = genero;
-        this.idUsuario = UUID.randomUUID().toString();
+    public Usuario (String nombre, String primerApellido, String segundoApellido, String direccion, String telefono, String email, LocalDate fechaNacimiento, String genero){
         this.nombre = nombre;
         this.primerApellido = primerApellido;
         this.segundoApellido = segundoApellido;
+        this.direccion = direccion;
         this.telefono = telefono;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
     }
 
     //Metodos de usuario
+
+    //Get y Set de los atributos
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
     
 }
